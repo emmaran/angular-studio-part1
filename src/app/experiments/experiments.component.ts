56753e89bed1e3d17ed60decaec0c17b1c8d15af
Emma Randolph <emma@Emmas-MBP-2.attlocal.net>
2022-08-01 19:38:18 -0500

    Finished studio

---
+++ src/app/experiments/experiments.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-experiments',
+  templateUrl: './experiments.component.html',
+  styleUrls: ['./experiments.component.css']
+})
+export class ExperimentsComponent implements OnInit {
+  listName = "Experiments";
+  listElements = ["Mars soil sample", "Plant growth in habitat", "Human bone density"];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
